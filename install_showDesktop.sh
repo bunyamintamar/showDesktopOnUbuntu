b46770b7b5007d8a7a2e0f7140db7331981e9104
Bunyamin TAMAR <bunyamintamar@yandex.com>
2021-12-14 11:25:00 +0300

    add installer and uninstaller

---
+++ install_showDesktop.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# STEP 1 - Enter your root password with 'sudo'
+echo
+sudo echo 'Installing the show desktop button!'
+echo '-----------------------------------'
+echo
+
+# STEP 2 - Download necessary files
+cd ~/Desktop/
+git clone https://github.com/bunyamintamar/showDesktopOnUbuntu.git
+cd showDesktopOnUbuntu/
+chmod +x showDesktop.sh
+sudo apt install xdotool -y
+sudo apt update -y
+
+# STEP 3 - Create application folder
+sudo mkdir /opt/showDesktop
+sudo cp showDesktop.sh /opt/showDesktop/
+sudo cp desktop.ico    /opt/showDesktop/
+
+# STEP 4 - Create shortcut
+touch showDesktop.desktop
+chmod +x showDesktop.desktop
+echo '#!/usr/bin/env xdg-open'                 >  showDesktop.desktop
+echo ' '                                       >> showDesktop.desktop
+echo '[Desktop Entry]'                         >> showDesktop.desktop
+echo 'Show Desktop'                            >> showDesktop.desktop
+echo 'StartupNotify=true'                      >> showDesktop.desktop
+echo 'Terminal=false'                          >> showDesktop.desktop
+echo 'Type=Application'                        >> showDesktop.desktop
+echo 'Icon=/opt/showDesktop/desktop.ico'       >> showDesktop.desktop
+echo 'Exec=/opt/showDesktop/showDesktop.sh %F' >> showDesktop.desktop
+sudo cp showDesktop.desktop /usr/share/applications/
+
+# STEP 5 - Add to favorities
+gsettings get /usr/share/applications/showDesktop.desktop favorite-apps
+
+# STEP 6 - Delete unnecessary files
+rm -r ~/Desktop/showDesktopOnUbuntu
+
+# ------------------------------------
+echo
+echo '-------------------------------'
+echo 'Bunyamin TAMAR - December 2021'
+echo 'linkedin.com/in/bunyamintamar'
+echo '-------------------------------'
+echo
---
+++ uninstall_showDesktop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo rm -r /opt/showDesktop
+sudo rm /usr/share/applications/showDesktop.showDesktop
+echo
+echo OK
+echo
